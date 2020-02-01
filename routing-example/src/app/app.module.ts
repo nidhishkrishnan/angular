08d332461e65953c0cd5b5859ec2e8464c7102cf
nidhishkrishnan <nidhishkrishnan@gmail.com>
2020-02-01 22:42:31 +0100

    Adding routing modules

--- routing-example/src/app/app.module.ts
+++ routing-example/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PlayersComponent } from './players/players.component';
+import { PlayerDetailsComponent } from './player-details/player-details.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {AppRoutingModule} from './app.routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayersComponent,
+    PlayerDetailsComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
