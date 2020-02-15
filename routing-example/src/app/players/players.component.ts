e41a42e7d518067ec5e89a990f321f02e5160410
nidhishkrishnan <nidhishkrishnan@gmail.com>
2020-02-15 22:21:30 +0100

    Retrieving params

--- routing-example/src/app/players/players.component.ts
+++ routing-example/src/app/players/players.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-players',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    console.log(this.route.snapshot.queryParams);
+    console.log(this.route.snapshot.fragment);
+    this.route.queryParams.subscribe();
+    this.route.fragment.subscribe();
   }
 
 }
