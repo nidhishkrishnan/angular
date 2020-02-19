0f42428ba8c2cc41ca9724fb75c33165e0fa72ce
Nidhish Krishnan <nidhishkrishnan@gmail.com>
2020-02-19 23:46:03 +0100

    Adding routing scripts

--- routing-example/src/app/app.routing.module.ts
+++ routing-example/src/app/app.routing.module.ts
@@ -3,13 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {PlayersComponent} from './players/players.component';
-import {PlayerDetailsComponent} from './player-details/player-details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'detail/:id', component: PlayerDetailsComponent },
-  { path: 'players', component: PlayersComponent }
+  { path: 'players', component: PlayersComponent },
+  { path: 'players/:id/view', component: PlayersComponent },
 ];
 
 @NgModule({
