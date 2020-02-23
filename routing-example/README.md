8be37b242251860bcd26422d507108d649202d58
Nidhish Krishnan <nidhishkrishnan@gmail.com>
2020-02-23 22:43:23 +0100

    Adding more function for retrieving params

--- routing-example/README.md
+++ routing-example/README.md
@@ -62,3 +62,36 @@ viewPlayers(id): void {
                        fragment: 'testing'});
 }
 ```
+
+
+### Retrieving Query Parameters (queryParams, fragment, path-params)
+
+```javascript
+constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    console.log(this.route.snapshot.queryParams); // for getting the queryParams values
+    console.log(this.route.snapshot.fragment); // for getting the fragment values
+
+    const id = +this.route.snapshot.params['id']; // converting string to number
+    console.log('id: ', id);
+
+    this.route.queryParams.subscribe(
+      (params: Params) => {
+        console.log('changed queryParams :', params);
+      }
+    );
+
+    this.route.fragment.subscribe(
+      (fragment: string) => {
+        console.log('changed fragment :', fragment);
+      }
+    );
+
+    this.route.params.subscribe(
+      (pathParams: Params) => {
+        console.log('changed path params :', pathParams);
+      }
+    );
+  }
+```
--- routing-example/src/app/players/players.component.ts
+++ routing-example/src/app/players/players.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-players',
@@ -11,10 +11,29 @@ export class PlayersComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-    console.log(this.route.snapshot.queryParams);
-    console.log(this.route.snapshot.fragment);
-    this.route.queryParams.subscribe();
-    this.route.fragment.subscribe();
+    console.log(this.route.snapshot.queryParams); // for getting the queryParams values
+    console.log(this.route.snapshot.fragment); // for getting the fragment values
+
+    const id = +this.route.snapshot.params['id']; // converting string to number
+    console.log('id: ', id);
+
+    this.route.queryParams.subscribe(
+      (params: Params) => {
+        console.log('changed queryParams :', params);
+      }
+    );
+
+    this.route.fragment.subscribe(
+      (fragment: string) => {
+        console.log('changed fragment :', fragment);
+      }
+    );
+
+    this.route.params.subscribe(
+      (pathParams: Params) => {
+        console.log('changed path params :', pathParams);
+      }
+    );
   }
 
 }
